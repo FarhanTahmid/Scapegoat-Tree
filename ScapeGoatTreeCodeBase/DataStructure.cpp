03afa74e851f48249dd7052a6f67ff2ee313f600
Farhan Tahmid <farhantahmid881@gmail.com>
2022-05-13 12:34:50 +0600

    ScapeGoat Tree methods

--- ScapeGoatTreeCodeBase/DataStructure.cpp
+++ ScapeGoatTreeCodeBase/DataStructure.cpp
@@ -1,23 +1,58 @@
 #include<iostream>
 #include "TreeNode.cpp"
 #include "DataStructure.h"
+#include<list>
+#include<math.h>
 using namespace std;
 
 template<typename ItemType>
 DataStructure<ItemType>::DataStructure(){
     root=NULL;
+    n=0;
 }
+
+template<typename ItemType>
+bool
+DataStructure<ItemType>::isEmpty(){
+    return root==NULL;
+}
+template<typename ItemType>
+int //count number of nodes of a parent
+DataStructure<ItemType>::size(TreeNode<ItemType>& tree){
+    if(tree==NULL){
+        return 0;
+    }else{
+        int size;
+        size+=size(tree->left);
+        size+=size(tree.right);
+        return size;
+    }
+}
+
+template<typename ItemType>
+int
+DataStructure<ItemType>::sizeofTree(){
+    return n;
+}
+
+template<typename ItemType>
+const int
+DataStructure<ItemType>::log32(int number){
+    double const log23=2.4663034623764317;
+    return (int)ceil(log23*log(q));
+}
+template<typename ItemType>
+template<typename ItemType>
+template<typename ItemType>
+template<typename ItemType>
+template<typename ItemType>
+
+
+
+
 template<typename ItemType>
 void 
 DataStructure<ItemType>::insertValue(ItemType,ItemType){
-     
+    
 }
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
-template<typename ItemType>
+
--- ScapeGoatTreeCodeBase/DataStructure.h
+++ ScapeGoatTreeCodeBase/DataStructure.h
@@ -1,14 +1,20 @@
 #ifndef DATASTRUCTURE_H_INCLUDED
 #define DATASTRUCTURE_H_INCLUDED
 #include "TreeNode.cpp"
+#include <list>
 template <typename ItemType>
 
 class DataStructure
 {
 public:
     TreeNode<ItemType> *root=new TreeNode<ItemType>;
-    
+    int n; //number of items in the data structure
+    int q; //overestimate of n
     DataStructure();
+    static int const log32(int);
+    bool isEmpty();
+    int size(TreeNode<ItemType>&);
+    int sizeofTree();
     void insertValue(ItemType,ItemType);
     void deleteItem();
     void searchItem();
